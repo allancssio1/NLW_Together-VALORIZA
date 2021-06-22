ecaf4d52cbe9153059939c3641b464ba4ae11d30
Allan Freitas <allan.cassio1@gmail.com>
2021-06-22 14:53:04 -0300

    started class 2 of nlw

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
+import 'reflect-metadata'
 import express from "express"
 
 const app = express()
-
-app.get('/', (req, res) => {
-  return res.send('olá')
-})
-
-app.post('/post', (req, res) => res.send('teste'))
 
 app.listen(3000, () => console.log('server is runnig'))
