ace930fb34d7093038b3d58d74a4fa51f1e798cf
Allan Freitas <allan.cassio1@gmail.com>
2021-07-01 07:57:06 -0300

    created routes from lists

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateComplimentController } from "./controllers/CreateComplimenrtController";
 import { CreateTagController } from "./controllers/CreateTagConstroller";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ListUserReceiveComplimentController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { ensureAuthenticate } from "./middlewares/ensureAuthenticate";
 
@@ -11,11 +13,15 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserSendComplimentsController = new ListUserSendComplimentsController()
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags",ensureAuthenticate , ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
 router.post("/compliments", ensureAuthenticate, createComplimentController.handle)
 
+router.get('/users/compliments/send', listUserSendComplimentsController.handle)
+router.get('/users/compliments/receive', listUserReceiveComplimentsController.handle)
 
 export {router}
